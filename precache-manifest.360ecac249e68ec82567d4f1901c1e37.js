8b8374bd07e1cc14528bc3f97e67ffb95031de0b
gayu <gayu1raja@gmail.com>
2020-07-31 16:25:41 +0530

    Updates

---
+++ precache-manifest.360ecac249e68ec82567d4f1901c1e37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317013c56d6109a0ac27eeaf45f00bd4",
+    "url": "/reactmaxeon/index.html"
+  },
+  {
+    "revision": "0c07d8bae5471ef06f50",
+    "url": "/reactmaxeon/static/css/main.c85cbdf9.chunk.css"
+  },
+  {
+    "revision": "437230b23a6d2dafc471",
+    "url": "/reactmaxeon/static/js/2.9b4228cc.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/reactmaxeon/static/js/2.9b4228cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c07d8bae5471ef06f50",
+    "url": "/reactmaxeon/static/js/main.323b4a52.chunk.js"
+  },
+  {
+    "revision": "63e1f1486014b9d3c271",
+    "url": "/reactmaxeon/static/js/runtime-main.980c3850.js"
+  },
+  {
+    "revision": "4a6ebd5ac6c39df826439199c960c8ef",
+    "url": "/reactmaxeon/static/media/Banner.4a6ebd5a.png"
+  }
+]);
